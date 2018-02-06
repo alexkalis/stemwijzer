3bfae10cf3011c6920b190a53008ee530aa5157d
alex kalis <99039736@mydavinci.nl>
2018-02-06 11:32:06 +0100

    Files toegevoegd. Css tot nu toe af.

---
+++ stemwijzer.js
@@ -0,0 +1,6 @@
+document.getElementById('start-begin');
+document.getElementById('image') = 'img/'
+function start() {
+document.getElementById('start-begin').innerHTML ='start' ;
+document.getElementById('image')
+}
